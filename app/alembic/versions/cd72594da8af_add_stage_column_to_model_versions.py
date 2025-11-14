063bc57dd85cc5bd33275d55b7ceefc23b172ae8
Jose Huerta <jlhuerta@uc.cl>
2025-11-14 17:56:19 -0300

    style: format code with Black

--- app/alembic/versions/cd72594da8af_add_stage_column_to_model_versions.py
+++ app/alembic/versions/cd72594da8af_add_stage_column_to_model_versions.py
@@ -5,25 +5,25 @@ Revises: 6839f4efda73
 Create Date: 2025-11-14 17:45:53.361714
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = 'cd72594da8af'
-down_revision: Union[str, Sequence[str], None] = '6839f4efda73'
+revision: str = "cd72594da8af"
+down_revision: Union[str, Sequence[str], None] = "6839f4efda73"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade():
     op.add_column(
-        'model_versions',
-        sa.Column('stage', sa.String(length=10), nullable=False, server_default='dev')
+        "model_versions",
+        sa.Column("stage", sa.String(length=10), nullable=False, server_default="dev"),
     )
 
 
 def downgrade():
-    op.drop_column('model_versions', 'stage')
+    op.drop_column("model_versions", "stage")
